c79dcec05f12e20b95ab1f54055f3cccc29e965f
IrishEgie <irishegie.arao@ssu.edu.ph>
2024-10-26 14:19:37 +0800

    first: creating starting code for text to morse code

---
+++ main.py
@@ -0,0 +1,10 @@
+import art
+
+program_title = art.title
+print('='*80)
+print(program_title)
+print('='*80)
+
+text_input = input('Write a sentence to convert to morse code, keep it short: \n')
+
+
